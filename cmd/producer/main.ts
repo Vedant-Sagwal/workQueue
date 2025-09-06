3e09806d3d3cbbe8c01c2e9d0acdf34aa972653e
Vedant-Sagwal <vedantsagwal@gmail.com>
2025-09-06 13:29:47 +0530

    Doing Cool Stuff

---
+++ cmd/producer/main.ts
@@ -0,0 +1,1 @@
+import { Task, Metrices } from "../../internal/task/task"
---
+++ internal/worker/worker.ts
@@ -0,0 +1,24 @@
+import { Task, Metrices } from "../task/task"
+
+function ProcessTask(taskToExecute: Task) {
+  if (taskToExecute == null) {
+    return Error("Payload is Empty")
+  }
+  switch (taskToExecute.type) {
+    case "send_email":
+      setTimeout(() => {
+        console.log(`Sending email to : ${taskToExecute.payload["to"]} with subject ${taskToExecute.payload["subject"]}`);
+      }, 1000);
+      return ;
+    case "resize_image":
+      console.log(`Resizing image to x coordinate : ${taskToExecute.payload["new_x"]}, y coordinate : ${taskToExecute.payload["new_y"]}`);
+      return ;
+    case "generate_pdf" : 
+      console.log("Generating PDF..")
+      return ;
+    case "":
+      console.error("Task Type is empty")
+    default :
+      console.error("Unsupported Task")
+  }
+}
